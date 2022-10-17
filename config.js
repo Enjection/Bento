20fc1b0087b797a367fc28856fb22f1022f7a15b
Innokentii Mokin <enjection@ya.ru>
2022-10-18 01:35:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ const CONFIG = {
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '55.755',
-	defaultLongitude: '37.617',
+	defaultLatitude: '55.7522',
+	defaultLongitude: '37.6156',
 
 	// Autochange
 	autoChangeTheme: true,
